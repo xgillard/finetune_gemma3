3e3ffe1e76f4dbada19df270179f64c87fef3c19
Xavier Gillard (xavier.gillard@uclouvain.be) <gillardx@mbackf2.cism.ucl.ac.be>
2025-05-06 18:12:50 +0200

    manneback

---
+++ a100.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+###
+# This script must be launched with sbatch <newjob.sh>
+###
+
+# Submission script for Manneback
+#SBATCH --job-name=finetune-llama
+#SBATCH --time=05:00:00 # hh:mm:ss
+#
+## MORE INFO HERE:
+## https://www.cism.ucl.ac.be/doc/_contents/Computing/index.html?highlight=gpu#manneback-cluster
+#SBATCH --ntasks=2
+#SBATCH --gres="gpu:TeslaA100:1"
+#SBATCH --mem-per-cpu=102400 # 100GB
+#SBATCH --partition=gpu
+#SBATCH --mail-user=xavier.gillard@uclouvain.be
+#SBATCH --mail-type=ALL
+#SBATCH --output=a100.txt
+
+module --force purge
+module --ignore_cache load              \
+	releases/2023a                  \
+	poetry/1.5.1-GCCcore-12.3.0     \
+	SciPy-bundle/2023.07-gfbf-2023a \
+	PyTorch/2.1.2-foss-2023a-CUDA-12.1.1 
+
+. .venv/bin/activate
+poetry config virtualenvs.options.system-site-packages true --local
+srun poetry run python -m finetune_gemma3.anonymize
+srun poetry run python -m finetune_gemma3.train
+
